05283fa34a962a60044605183707086b9f8b5804
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-03 15:43:15 +0300

    add entities

---
+++ InnoClinic.OfficesMicroservice/Domain/Entities/Office.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Office : BaseEntity
+    {
+        public string Address { get; set; }
+        public Guid PhotoId { get; set; }
+        public string RegistryPhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
