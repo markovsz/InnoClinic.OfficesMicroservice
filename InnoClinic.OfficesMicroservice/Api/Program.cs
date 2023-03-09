38159896c5a987dd26cc236b6a0f712ebfa985e0
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-09 16:38:14 +0300

    add db settings to di

--- InnoClinic.OfficesMicroservice/Api/Program.cs
+++ InnoClinic.OfficesMicroservice/Api/Program.cs
@@ -4,6 +4,7 @@ using Application;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureDb(builder.Configuration);
 builder.Services.ConfigureRepositories();
 builder.Services.ConfigureMediatR();
 builder.Services.AddControllers();
