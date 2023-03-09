f43d4423d2141d256fe249c9ec525307ab14bbc7
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-09 17:10:52 +0300

    add CreateOfficeCommand

---
+++ InnoClinic.OfficesMicroservice/Application/Commands/CreateOffice/CreateOfficeCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Messaging;
+using MediatR;
+
+namespace Application.Commands.CreateOffice
+{
+    public class CreateOfficeCommand : ICommand<Guid>
+    {
+        public string Address { get; set; }
+        public Guid PhotoId { get; set; }
+        public string RegistryPhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+
+        public CreateOfficeCommand(string Address, Guid PhotoId, string RegistryPhoneNumber, bool IsActive)
+        {
+            this.Address = Address;
+            this.PhotoId = PhotoId;
+            this.RegistryPhoneNumber = RegistryPhoneNumber;
+            this.IsActive = IsActive;
+        }
+    }
+}
---
+++ InnoClinic.OfficesMicroservice/Application/Commands/CreateOffice/CreateOfficeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Messaging;
+using AutoMapper;
+using Domain.Abstractions.CommandRepositories;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands.CreateOffice
+{
+    public class CreateOfficeCommandHandler : ICommandHandler<CreateOfficeCommand, Guid>
+    {
+        private IReadWriteOfficesRepository _officesRepository;
+        private IMapper _mapper;
+
+        public CreateOfficeCommandHandler(IReadWriteOfficesRepository officesRepository, IMapper mapper)
+        {
+            _officesRepository = officesRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Handle(CreateOfficeCommand request, CancellationToken cancellationToken)
+        {
+            var office = _mapper.Map<Office>(request);
+            await _officesRepository.CreateAsync(office);
+            return office.Id;
+        }
+    }
+}
