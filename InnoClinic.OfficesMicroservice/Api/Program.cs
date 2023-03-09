f5934908e3c3bb562067fa7b1de722b754893fa7
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-09 16:56:55 +0300

    add automapper

--- InnoClinic.OfficesMicroservice/Api/Program.cs
+++ InnoClinic.OfficesMicroservice/Api/Program.cs
@@ -1,11 +1,13 @@
 using Api.Extensions;
 using Application;
+using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.ConfigureDb(builder.Configuration);
 builder.Services.ConfigureRepositories();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.ConfigureMediatR();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
---
+++ InnoClinic.OfficesMicroservice/Infrastructure/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Commands.CreateOffice;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<CreateOfficeCommand, Office>();
+        }
+    }
+}
