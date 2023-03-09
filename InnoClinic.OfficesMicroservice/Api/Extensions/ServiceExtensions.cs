f6fd7257102d17f267b09abb108ff6fb075d26b1
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-09 15:18:09 +0300

    add mongodb

---
+++ InnoClinic.OfficesMicroservice/Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Settings;
+using Microsoft.Extensions.Options;
+
+namespace Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<OfficesDbSettings>(
+                configuration.GetSection(nameof(OfficesDbSettings)));
+            services.AddSingleton<IOfficesDbSettings>(provider =>
+                provider.GetRequiredService<IOptions<OfficesDbSettings>>().Value);
+        }
+    }
+}
---
+++ InnoClinic.OfficesMicroservice/Infrastructure/Settings/OfficesDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Settings
+{
+    public class OfficesDbSettings : IOfficesDbSettings
+    {
+        public string OfficesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
