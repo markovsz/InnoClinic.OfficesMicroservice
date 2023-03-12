67c5f5e34b30f78c4ba5ab253d58ae0cb617853e
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-12 22:53:16 +0300

    add ChangeOfficeStatusCommand

---
+++ InnoClinic.OfficesMicroservice/Application/Commands/ChangeOfficeStatus/ChangeOfficeStatusCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Enums;
+using System.Runtime.Serialization;
+
+namespace Application.Commands.ChangeOfficeStatus
+{
+    public class ChangeOfficeStatusCommand : ICommand
+    {
+        [IgnoreDataMember]
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public ChangeOfficeStatusCommand(Guid id, string status) 
+        {
+            Id = id;
+            Status = status;
+        }
+    }
+}
---
+++ InnoClinic.OfficesMicroservice/Application/Commands/ChangeOfficeStatus/ChangeOfficeStatusCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Abstractions.CommandRepositories;
+
+namespace Application.Commands.ChangeOfficeStatus
+{
+    public class ChangeOfficeStatusCommandHandler : ICommandHandler<ChangeOfficeStatusCommand>
+    {
+        private IReadWriteOfficesRepository _officesRepository;
+        public ChangeOfficeStatusCommandHandler(IReadWriteOfficesRepository officesRepository)
+        {
+            _officesRepository = officesRepository;
+        }
+
+        public async Task Handle(ChangeOfficeStatusCommand request, CancellationToken cancellationToken)
+        {
+            var office = await _officesRepository.GetByIdAsync(request.Id);
+            office.Status = request.Status;
+            await _officesRepository.UpdateAsync(office);
+        }
+    }
+}
