51fc4ddaff073e0558877ca45cda00d709bf4376
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-12 22:55:00 +0300

    add UpdateOfficeCommand

---
+++ InnoClinic.OfficesMicroservice/Application/Commands/UpdateOffice/UpdateOfficeCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.Abstractions.Messaging;
+using System.Runtime.Serialization;
+
+namespace Application.Commands.UpdateOffice
+{
+    public class UpdateOfficeCommand : ICommand
+    {
+        [IgnoreDataMember]
+        public Guid Id { get; set; }
+        public Guid PhotoId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string OfficeNumber { get; set; }
+        public string RegistryPhoneNumber { get; set; }
+        public string Status { get; set; }
+
+        public UpdateOfficeCommand(Guid id, Guid photoId, string city, string street, string houseNumber, string officeNumber, string registryPhoneNumber, string status)
+        {
+            Id = id;
+            PhotoId = photoId;
+            City = city;
+            Street = street;
+            HouseNumber = houseNumber;
+            OfficeNumber = officeNumber;
+            RegistryPhoneNumber = registryPhoneNumber;
+            Status = status;
+        }
+    }
+}
---
+++ InnoClinic.OfficesMicroservice/Application/Commands/UpdateOffice/UpdateOfficeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions.Messaging;
+using AutoMapper;
+using Domain.Abstractions.CommandRepositories;
+using Domain.Entities;
+
+namespace Application.Commands.UpdateOffice
+{
+    public class UpdateOfficeCommandHandler : ICommandHandler<UpdateOfficeCommand>
+    {
+        private IReadWriteOfficesRepository _officesRepository;
+        private IMapper _mapper;
+        public UpdateOfficeCommandHandler(IReadWriteOfficesRepository officesRepository, IMapper mapper)
+        {
+            _officesRepository = officesRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(UpdateOfficeCommand request, CancellationToken cancellationToken)
+        {
+            var office = _mapper.Map<Office>(request);
+            await _officesRepository.UpdateAsync(office);
+        }
+    }
+}
