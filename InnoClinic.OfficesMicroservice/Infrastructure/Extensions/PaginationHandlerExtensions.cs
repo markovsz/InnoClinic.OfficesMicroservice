fd68419f31c1e7fbfb14b74923232ff11d9fae58
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-23 17:55:43 +0300

    add pagination

---
+++ InnoClinic.OfficesMicroservice/Infrastructure/Extensions/PaginationHandlerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Domain.RequestParameters;
+using MongoDB.Driver.Linq;
+
+namespace Infrastructure.Extensions
+{
+    public static class PaginationHandlerExtensions
+    {
+        public static IMongoQueryable<TEntity> ApplyPagination<TEntity>(this IMongoQueryable<TEntity> entities, PaginationParameters parameters)
+        {
+            entities = entities
+                .Skip((parameters.PageSize - 1) * parameters.Page)
+                .Take(parameters.PageSize);
+            return entities;
+        }
+    }
+}
